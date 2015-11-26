f0bb42ddd1ba7e0e9ff1976e2945536a30b6519b
Julio César Barrera Andrade <juliobarrera@conlana.com>
2015-11-25 20:04:20 -0600

    Archivo de definicion de clase para Conlana

---
+++ conlana/conlana.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for container
+// Project: http://www.conlana.com/
+// Definitions by: Julio Barrera <https://github.com/codebitmx/>, Julio César Barrera
+// Definitions: https://github.com/codebitmx/DefinitelyTyped
+
+declare module Conlana.Widespread {
+	interface Templates {
+		Collection: any[];
+	}
+}
